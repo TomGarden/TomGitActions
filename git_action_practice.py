3bd2bd0454a313a88aeed6e6cdf717e6b59e7c64
tom <01wukong10@giaml.com>
2020-03-30 21:29:09 +0800

    首次测试执行 actions

---
+++ git_action_practice.py
@@ -0,0 +1,15 @@
+import os
+import re
+import json
+import pathlib
+import requests
+import subprocess
+import urllib.parse
+from github import Github
+from github import GithubException
+from github import UnknownObjectException
+
+GITHUB_API = "https://api.github.com"
+GITHUB_ACTION_NAME = os.environ['GITHUB_ACTION']
+
+print(os.environ)
