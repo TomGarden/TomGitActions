c0cb4bfbe258c947fce1ef6ad0c277f0e2586edc
tom <01wukong10@giaml.com>
2020-05-26 22:10:47 +0800

    试试 Ubuntu 的表现

---
+++ .github/github_actions/install_dependence_ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -xv
+
+# 校验首个参数所代表的 app 是否已经安装了，如果没有尚未安装就安装它
+# 当前不足之处在于无法返回安装结果，默认为安装成功
+function install_one_app() {
+  if [ -x `command -v $1` ]; then
+    echo "「$1」 has installed"
+  else
+    printf "begin install 「%s」 ......................................................... \n" "$1"
+    sudo apt-get install "$1"
+    printf "install 「%s」 DONE ......................................................... \n" "$1"
+  fi
+}
+
+# 校验所有给定的参数，如果代表的应用没有安装就尝试安装
+function install_apps() {
+  for app in "$@"; do
+    install_one_app "$app"
+  done
+}
+
+# 安装软件
+echo 'install jq python3 .......................................'
+install_apps  jq  python3
+echo 'python3 -m ensurepip .......................................'
+python3 -m ensurepip
+echo 'pip3 --no-cache-dir install --upgrade pip ..................'
+pip3 --no-cache-dir install --upgrade pip
+echo 'pip3 install requests PyGithub pathlib .....................'
+pip3 install requests PyGithub pathlib
