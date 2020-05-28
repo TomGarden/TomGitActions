4e81bb2487b6ebb31591cc44ef6c801b8c3795d1
tom <01wukong10@giaml.com>
2020-05-28 21:47:05 +0800

    原文链接修正

--- .github/github_actions/git_action_practice.py
+++ .github/github_actions/git_action_practice.py
@@ -350,11 +350,9 @@ def issue_opt(new_file: str, old_file: str = None):
     with open(file_desc, encoding='utf-8', mode='r') as file_stream:
         _issue_body = file_stream.read()
         _issue_body = replace_markdown_links(_issue_body, file_desc.parent.as_posix())
-        _issue_warring = "> [正则匹配链接`[]()`语法上有局限性 , 如有疑虑可查看**本文**原文](https://github.com/{GITHUB_USER}/{" \
-                         "GITHUB_REPO}/blob/{GITHUB_BRANCH}/{file})".format(GITHUB_USER=GITHUB_USER,
-                                                                            GITHUB_REPO=GITHUB_REPO,
-                                                                            GITHUB_BRANCH=GITHUB_BRANCH,
-                                                                            file=file_desc.as_posix())
+        _issue_warring = "> [正则匹配链接`[]()`语法上有局限性 , 如有疑虑可查看**本文**原文](https://github.com/{GITHUB_REPO}/blob/{" \
+                         "GITHUB_BRANCH}/{file})".format(GITHUB_USER=GITHUB_USER, GITHUB_REPO=GITHUB_REPO,
+                                                         GITHUB_BRANCH=GITHUB_BRANCH, file=file_desc.as_posix())
         _issue_header = read_file_text(ISSUES_HEADER_PATH)
         _issue_footer = read_file_text(ISSUES_FOOTER_PATH)
         _issue_body = _issue_header + _issue_warring + "\n\n" + _issue_body + _issue_footer
